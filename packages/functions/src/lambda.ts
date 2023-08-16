296885df26509f3a1de241d716d020a69c5746e2
Alan Shaw <alan.shaw@protocol.ai>
2023-08-16 10:04:09 +0200

    feat: json response

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -11,22 +11,22 @@ export const handler = ApiHandler(async event => {
   const { searchParams } = new URL(`http://localhost/?${event.rawQueryString}`)
 
   const region = searchParams.get('region')
-  if (!region) return { statusCode: 400, body: 'Missing "region" search parameter' }
-  if (!['us-east-1', 'us-west-2'].includes(region)) return { statusCode: 400, body: 'Invalid region' }
+  if (!region) return errorResponse('Missing "region" search parameter', 400)
+  if (!['us-east-1', 'us-west-2'].includes(region)) return errorResponse('Invalid region', 400)
 
   const bucket = searchParams.get('bucket')
-  if (!bucket) return { statusCode: 400, body: 'Missing "bucket" search parameter' }
-  if (!bucket.startsWith('dotstorage')) return { statusCode: 400, body: 'Invalid bucket' }
+  if (!bucket) return errorResponse('Missing "bucket" search parameter', 400)
+  if (!bucket.startsWith('dotstorage')) return errorResponse('Invalid bucket', 400)
 
   const key = searchParams.get('key')
-  if (!key) return { statusCode: 400, body: 'Missing "key" search parameter' }
-  if (!key.endsWith('.car')) return { statusCode: 400, body: 'Only keys for CARs supported' }
+  if (!key) return errorResponse('Missing "key" search parameter', 400)
+  if (!key.endsWith('.car')) return errorResponse('Only keys for CARs supported', 400)
 
   const s3 = new S3Client({ region })
   const cmd = new GetObjectCommand({ Bucket: bucket, Key: key })
 
   const res = await s3.send(cmd)
-  if (!res.Body) return { statusCode: 404, body: 'Object not found' }
+  if (!res.Body) return errorResponse('Object not found', 404)
 
   const hash = crypto.createHash('sha256')
   await res.Body.transformToWebStream()
@@ -35,5 +35,10 @@ export const handler = ApiHandler(async event => {
   const digest = Digest.create(sha256.code, hash.digest())
   const cid = Link.create(CAR_CODEC, digest)
 
-  return { statusCode: 200, body: `{"/":"${cid}"}` }
+  return { statusCode: 200, body: `{"ok":true,"cid":{"/":"${cid}"}}` }
+})
+
+const errorResponse = (message: string, statusCode = 500) => ({
+  statusCode,
+  body: JSON.stringify({ ok: false, error: message })
 })
