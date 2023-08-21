bc0567dbe27bc9c5196fe91e0bed5feb963ffed1
Alan Shaw <alan.shaw@protocol.ai>
2023-08-21 16:27:07 +0200

    feat: add copy command

--- packages/cli/bin.js
+++ packages/cli/bin.js
@@ -8,6 +8,7 @@ import { Parse } from 'ndjson-web'
 import * as dagJSON from '@ipld/dag-json'
 import { Parallel } from 'parallel-transform-web'
 import retry from 'p-retry'
+import * as Link from 'multiformats/link'
 
 const pkg = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url)).toString())
 const cli = sade('sha256it')
@@ -61,7 +62,7 @@ cli
   .option('-r, --region', 'Bucket region.')
   .option('-b, --bucket', 'Bucket name.')
   .action(async (/** @type {string|undefined} */ key, /** @type {Record<string, string|undefined>} */ options) => {
-    const endpoint = new URL(process.env.SERVICE_ENDPOINT ?? notNully(options, 'endpoint', 'missing required option'))
+    const endpoint = new URL(options.endpoint ?? notNully(process.env, 'SERVICE_ENDPOINT', 'missing required option'))
     if (key) {
       const region = notNully(options, 'region', 'missing required option')
       const bucket = notNully(options, 'bucket', 'missing required option')
@@ -106,6 +107,88 @@ const hash = async (endpoint, region, bucket, key) => {
   const text = await res.text()
   if (!res.ok) throw new Error(`hash failed: ${text}`)
   return dagJSON.parse(text)
+}
+
+cli.command('copy [key] [cid]')
+  .option('--root', 'DAG root CID (if not derivable from key).')
+  .option('-e, --endpoint', 'Service endpoint.')
+  .option('-r, --region', 'Bucket region.')
+  .option('-b, --bucket', 'Bucket name.')
+  .action(async (/** @type {string|undefined} */ key, /** @type {string|undefined} */ cidstr, /** @type {Record<string, string|undefined>} */ options) => {
+    const endpoint = new URL(options.endpoint ?? notNully(process.env, 'COPY_SERVICE_ENDPOINT', 'missing required option'))
+    if (key && cidstr) {
+      const region = notNully(options, 'region', 'missing required option')
+      const bucket = notNully(options, 'bucket', 'missing required option')
+      const root = options.root ? Link.parse(options.root) : bucketKeyToRootCID(key)
+      if (!root) throw new Error('missing required option: root')
+      const cid = Link.parse(cidstr)
+      try {
+        // @ts-expect-error
+        await copy(endpoint, region, bucket, key, cid, root)
+        return console.log(dagJSON.stringify({ region, bucket, key, cid, root }))
+      } catch (err) {
+        console.warn(`failed copy of ${region}/${bucket}/${key}`, err)
+        return console.log(dagJSON.stringify({ region, bucket, key, cid, root, error: err.message }))
+      }
+    }
+
+    const source = /** @type {ReadableStream<Uint8Array>} */ (Readable.toWeb(process.stdin))
+    await source
+      .pipeThrough(/** @type {Parse<{ region?: string, bucket?: string, key: string, cid: { '/': string }, root?: { '/': string } }>} */ (new Parse()))
+      .pipeThrough(new Parallel(concurrency, async item => {
+        const region = item.region ?? notNully(options, 'region', 'missing required option')
+        const bucket = item.bucket ?? notNully(options, 'bucket', 'missing required option')
+        const { key } = item
+        const cid = Link.parse(item.cid['/'])
+        const root = item.root
+          ? Link.parse(item.root['/'])
+          : options.root
+          ? Link.parse(options.root)
+          : bucketKeyToRootCID(key)
+        if (!root) throw new Error('missing required option: root')
+        try {
+          // @ts-expect-error
+          await retry(() => copy(endpoint, region, bucket, key, cid, root))
+          return { region, bucket, key, cid, root }
+        } catch (err) {
+          console.warn(`failed copy of ${region}/${bucket}/${key}`, err)
+          return { region, bucket, key, cid, root, error: err.message }
+        }
+      }))
+      .pipeThrough(new TransformStream({
+        transform: (item, controller) => controller.enqueue(`${dagJSON.stringify(item)}\n`)
+      }))
+      .pipeTo(Writable.toWeb(process.stdout))
+  })
+
+/**
+ * @param {URL} endpoint
+ * @param {string} region
+ * @param {string} bucket
+ * @param {string} key
+ * @param {import('multiformats').Link} shard
+ * @param {import('multiformats').UnknownLink} root
+ */
+const copy = async (endpoint, region, bucket, key, shard, root) => {
+  const url = new URL(endpoint)
+  url.searchParams.set('region', region)
+  url.searchParams.set('bucket', bucket)
+  url.searchParams.set('key', key)
+  url.searchParams.set('shard', shard.toString())
+  url.searchParams.set('root', root.toString())
+  const res = await fetch(url)
+  const text = await res.text()
+  if (!res.ok) throw new Error(`copy failed: ${text}`)
+  return dagJSON.parse(text)
+}
+
+/** @param {string} key */
+const bucketKeyToRootCID = key => {
+  if (key.startsWith('complete/')) {
+    const cidstr = key.split('/').pop()?.replace('.car', '')
+    if (!cidstr) return
+    return cidstr ? Link.parse(cidstr) : undefined
+  }
 }
 
 /**
